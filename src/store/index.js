9c2e096e8e3d3734dceed99c7b089239d2840b6c
clarencepenz <clarencepenz@users.noreply.github.com>
2020-04-28 16:29:51 +0100

    update initialized

---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware, compose} from 'redux'
+import thunk from 'redux-thunk'
+
+const initialState = {}
+const middleware = [thunk]
+
+const store = createStore(null, initialState, compose(applyMiddleware(...middleware), 
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
